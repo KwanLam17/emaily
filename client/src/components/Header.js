e3aeb281980146396482eff9841818eb482cc4a0
KwanLam <kwan.lam@alumni.ubc.ca>
2018-08-28 02:24:10 -0700

    finished login

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li><a href="/auth/google">Login with Google</a></li>
+                );
+            default:
+                return (
+                    <li><a>Logout</a></li>
+                );
+        }
+    }
+
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="left brand-logo">
+                        Emaily
+                    </a>
+                    <ul className="right">
+                        {this.renderContent()}
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
 
-export default Header;
+function mapStateToProps(state) {
+    return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Header);
