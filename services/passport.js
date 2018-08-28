f79dd9eec69bd32e95652436ff00d50a455736a1
KwanLam <kwan.lam@alumni.ubc.ca>
2018-08-28 00:18:26 -0700

    refractored async

--- services/passport.js
+++ services/passport.js
@@ -24,22 +24,17 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
+    }, 
+    async (accessToken, refreshToken, profile, done) => {
         //console.log('access token', accessToken);
         //console.log('refresh token', refreshToken);
         //console.log('profile:', profile);
-        User.findOne({ googleID: profile.id }).then((existingUser) => {
-            if(existingUser) {
-                // we already have a record of this User
-                done(null, existingUser);
-            }
-            else {
-                new User({ 
-                    googleID: profile.id 
-                }).save().then(user => done(null, user)); 
-            }
-        });
-        
-        
+        const existingUser = await User.findOne({ googleID: profile.id })
+        if(existingUser) {
+            // we already have a record of this User
+            return done(null, existingUser);
+        }    
+        const user = await new User({ googleID: profile.id }).save()
+        done(null, user); 
     })
 );
