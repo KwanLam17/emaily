4e69aebcbdc78b6bc8757c71245a142bc4f62f9c
KwanLam <kwan.lam@alumni.ubc.ca>
2018-08-30 13:56:41 -0700

    finished Mongo query

--- routes/surveyRoute.js
+++ routes/surveyRoute.js
@@ -1,19 +1,48 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
-
 const Survey = mongoose.model('surveys');
 
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting!');
     });
 
     app.post('/api/surveys/webhooks', (req, res) => {
-        console.log(req.body);
+        //console.log(req.body);
+        const p = new Path('/api/surveys/:surveyId/:choice');
+
+        _.chain(req.body)
+            .map(({ email, url }) => {
+                const match = p.test(new URL(url).pathname);
+                if (match) {
+                    return { email, surveyId: match.surveyId, choice: match.choice };
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne({
+                    _id: surveyId,
+                    recipients: {
+                        $elemMatch: { email: email, responded: false }
+                    }
+                }, {
+                    $inc: { [choice]: 1 },
+                    $set: { 'recipients.$.responded': true },
+                    lastResponded: new Date()
+                }).exec();
+            })
+            .value();
+
+        //console.log(events);
+
         res.send({});
     });
 
@@ -24,7 +53,7 @@ module.exports = app => {
             title,
             subject,
             body,
-            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+            recipients: recipients.split(',').map(email => ({ email: email.trim(), responded: false })),
             _user: req.user.id,
             dateSent: Date.now()  
             //lastResponded: Date.now
