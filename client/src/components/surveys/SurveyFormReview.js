f142b4e83a4766fee98dffb1faacd0ad042578a4
KwanLam <kwan.lam@alumni.ubc.ca>
2018-08-29 22:43:05 -0700

    added withRouter necessary? finished survey feature

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
 import * as actions from '../../actions/index';
+import { withRouter } from 'react-router-dom';
 
-const SurveyReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyReview = ({ onCancel, formValues, submitSurvey, history }) => {
     const reviewFields = _.map(formFields, ({ name, label }) => {
         return (
            <div key={name}>
@@ -27,7 +28,7 @@ const SurveyReview = ({ onCancel, formValues, submitSurvey }) => {
                 Back
             </button>
             <button 
-                onClick={() => submitSurvey(formValues)}
+                onClick={() => submitSurvey(formValues, history)}
                 className="green btn-flat right white-text"
             >
                 Send Survey
@@ -41,4 +42,4 @@ function mapStateToProps(state) {
     return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps, actions)(SurveyReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyReview));
